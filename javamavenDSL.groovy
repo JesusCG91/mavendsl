49c491631961c2bdfa45239ff18cf216011536bc
JesusCG91 <46775810+JesusCG91@users.noreply.github.com>
2022-01-21 09:24:22 +0100

    Update javamavenDSL.groovy

--- javamavenDSL.groovy
+++ javamavenDSL.groovy
@@ -1,7 +1,7 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
+        git('https://github.com/JesusCG91/mavendsl.git', 'master') { node ->
             node / gitConfigName('jcastrog0212')
             node / gitConfigEmail('jcastrog0212@gmail.com')
         }
